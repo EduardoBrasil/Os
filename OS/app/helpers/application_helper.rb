811e12e78862aba8b17b16ec662b03680ced69f1
Eduardo <brasil.eduardo1@gmail.com>
2014-12-13 21:17:36 -0200

    inicio do projeto

---
+++ OS/app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+module ApplicationHelper
+
+  def stylesheet(*args)
+	 content_for(:head) {stylesheet_link_tag(*args)}
+  end
+end
---
+++ OS/app/views/usuarios/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @usuario, :id, :name, :matricula, :created_at, :updated_at
